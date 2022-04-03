5e848e3304cf966def65c0e4616d1ca6115f3448
CommanderFoo <pixeldepth@gmail.com>
2022-04-04 00:55:09 +0100

    Update Total_Players_Online_Server.lua

--- Data/Scripts/Total_Players_Online_Server.lua
+++ Data/Scripts/Total_Players_Online_Server.lua
@@ -1,42 +1,34 @@
 local TOTAL_PLAYERS_KEY = script:GetCustomProperty("NodeLinkTotalPlayers")
 local DATA = script:GetCustomProperty("Data"):WaitForObject()
 
-local delta_players = 0
-local wait_time = 5
-
 local function update_clients(key, data)
 	DATA:SetCustomProperty("total_players", data.count or 1)
 end
 
 local function on_player_joined(player)
-	delta_players = delta_players + 1
+	Storage.SetConcurrentCreatorData(TOTAL_PLAYERS_KEY, function(data)
+		if(not data.count) then
+			data.count = 0
+		end
+	
+		data.count = data.count + 1
+	
+		return data
+	end)
+
 	update_clients(nil, Storage.GetConcurrentCreatorData(TOTAL_PLAYERS_KEY))
 end
 
 local function on_player_left(player)
-	delta_players = delta_players - 1
-end
-
-local function update_total_players(data)
-	if(not data.count) then
-		data.count = 0
-	end
-
-	data.count = data.count + delta_players
-
-	return data
-end
-
-function Tick()
-	Task.Wait(wait_time)
-
-	if delta_players == 0 or Storage.HasPendingSetConcurrentCreatorData(TOTAL_PLAYERS_KEY) then
-		return
-	end
-
-	Storage.SetConcurrentCreatorData(TOTAL_PLAYERS_KEY, update_total_players)
+	Storage.SetConcurrentCreatorData(TOTAL_PLAYERS_KEY, function(data)
+		if(not data.count) then
+			data.count = 0
+		else
+			data.count = data.count - 1
+		end
 	
-	delta_players = 0
+		return data
+	end)
 end
 
 Storage.ConnectToConcurrentCreatorDataChanged(TOTAL_PLAYERS_KEY, update_clients)
